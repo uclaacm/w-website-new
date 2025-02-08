d55a0c5d202c12ae23bb56c32243b28d9dd4581e
Arka Sharma <arkasharma2004@gmail.com>
2025-02-07 17:52:39 -0800

    Hardcoded events, members for sample website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
 import MainLayout from '../components/MainLayout';
-import getCommitteeInfo from '../scripts/landing-page-generator.mjs';
+//import getCommitteeInfo from '../scripts/landing-page-generator.mjs';
 import vars from '../styles/global_variables.module.scss';
 import styles from '../styles/LandingPage.module.scss';
 
@@ -40,13 +40,15 @@ export default function Home({ committee }: Props): JSX.Element {
               </h1>
               <h2 className={styles.lead}>{committee.subtitle}</h2>
             </div>
-            <p className={styles.description}>{committee.description}</p>
+            <img src="../public/w_hd.svg" />
             <a
               className={styles['cta-btn']}
               href="https://acm.cs.ucla.edu/internship"
             >
               Join Us
             </a>
+            <div className={styles.title}>What is {committee.committee}?</div>
+            <p className={styles.description}>{committee.description}</p>
           </div>
         </div>
       </div>
@@ -55,7 +57,20 @@ export default function Home({ committee }: Props): JSX.Element {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const committee = await getCommitteeInfo(vars.committee);
+  // const committee = await getCommitteeInfo(vars.committee);
+  const committee = {
+    committee: 'ACM-W',
+    name: 'Association for Computing Machinery',
+    subtitle: 'Empowering students for diversity and inclusion in tech',
+    description: 'Our mission is to support, celebrate, and advocate for anyone and everyone interested in computer science. We are dedicated to fostering an inclusive community by providing programs and resoirces to UCLA students. Whether you\'re a seasoned coder or just starting out, ACM-W is here to support you and your journey!',
+    logoLink: '/assets/acm-logo.png',
+    dcLink: 'https://discord.gg/acm-ucla',
+    igLink: 'https://www.instagram.com/acm_ucla/',
+    email: 'acm@cs.ucla.edu',
+    favicon: '/assets/favicon.ico',
+    backgroundImg: '/assets/acm-background.jpg',
+  };
+
   return {
     props: {
       committee: committee,
